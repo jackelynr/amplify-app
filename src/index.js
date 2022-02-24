5188a046d070b7649078432607a93e33aeb195d9
jackelynr <jrodriguez19@madisoncollege.edu>
2022-02-24 16:16:00 -0600

    Using the more recent ui-react components

--- src/index.js
+++ src/index.js
@@ -7,11 +7,16 @@ import reportWebVitals from './reportWebVitals';
 //importing & configuring AWS amplify
 import Amplify from 'aws-amplify';
 import config from './aws-exports';
+import {AmplifyProvider} from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
 Amplify.configure(config);
 
 ReactDOM.render(
   <React.StrictMode>
+  <AmplifyProvider>
     <App />
+  </AmplifyProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
